6468714c210d3ee2aa4e33aab3dca1dc3005ce93
Mårten Andersson <maarten.andersson@gmail.com>
2013-03-19 23:11:31 +0100

    added project

---
+++ StudioHalland/StudioHalland/Scripts/Views/Home/Index.js
@@ -0,0 +1,36 @@
+﻿
+var index = {
+    initialize: function () {
+
+        $("#container").isotope({
+            layoutMode: "masonry",
+            columnWidthInteger: 220,
+            itemSelector: '.element',
+
+        }, function ($items) {
+            var id = this.attr('id'),
+                len = $items.length;
+            console.log('Isotope has filtered for ' + len + ' items in #' + id);
+        });
+
+    }
+}
+
+
+
+$(function () {
+    var stepHeight = -1;
+
+    $(".element").each(function () {
+        if (stepHeight < 0) stepHeight = $(this).outerHeight();
+        $(this).height(function (i, h) {
+            return (stepHeight * Math.ceil($(this).outerHeight() / stepHeight));
+            //return Math.ceil(($(this).outerHeight() * 100) / 100);
+        }, $(this).outerHeight());
+
+    });
+
+    index.initialize();
+
+
+});
